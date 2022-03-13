71d37ae2f77f0c14eedb49c79b728a328ad8dc4d
AWONG <andrewwong.wcy@gmail.com>
2022-03-13 02:24:46 +0000

    Start Geotag modifier

--- exif_modifier.py
+++ exif_modifier.py
@@ -10,16 +10,31 @@ class ExifModifier:
         :param target_exif: Dictionary containing the target EXIF data to modify TO.
         """
         self._image_filepath = image_filepath
-        self._exif_dict = target_exif
+        self._target_exif_dict = target_exif
+        self._exif_dict = {}
+
+    def _modify_geodata(self) -> None:
+        """
+        Modify the geotag data
+        :return: None
+        """
+        gps_dict = {}
+        #
+        # 'GPSLatitude':
+
+
+
+
+    def _modify_main(self) -> None:
+        pass
+
 
     # ********* MAIN METHOD
     def modify(self) -> None:
         img = Image.open(self._image_filepath)
-        img_exif = img.getexif()
-        if img_exif is None:
-            log.debug('No existing EXIF data found.')
-
+        self._exif_dict = {
+            ExifTags.TAGS[k]: v for k, v in img.getexif().items() if k in ExifTags.TAGS
+        }
+        self._modify_geodata()
         t = ''
-
-
-
+        
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 import json
 import logging as log
+from collections import namedtuple
 from config import Config
 from exif_modifier import ExifModifier
 
 
 class Google2ICloudPhotos:
+    fpath_row = namedtuple('filepath_map', ['title', 'fpath_img', 'fpath_json'])
+
     def __init__(self, dir_google: str, dir_icloud: str):
         self._dir_google: str = dir_google
         self._dir_icloud: str = dir_icloud
 
         self._files_all = []
-        self._files_photos = []
-        self._map_title2json = {}
+        self._map_filepaths = {}
 
     def _check_extensions(self) -> None:
         """
@@ -30,52 +32,63 @@ class Google2ICloudPhotos:
         if len(unsupported) > 0:
             raise Exception(f'Unsupported Extensions: {unsupported}')
 
-    def _split_filepaths(self) -> None:
+    def _build_filepath_map(self) -> None:
         """
         Split the filepaths into photos and jsons
         :return: None
         """
-        log.info('Splitting filepaths...')
-        self._files_photos = [x for x in self._files_all if not x.upper().endswith('.JPG')]
-        json_files = [x for x in self._files_all if x.upper().endswith('.JSON')]
+        log.info('Mapping filepaths...')
 
-        for file in json_files:
-            filename = os.path.basename(file)
-            self._map_title2json[filename] = file
+        # Recall we have already checked all the extensions in Config.supported_extensions
+        fpaths_img = [x for x in self._files_all if not x.upper().endswith('.JPG')]
+        fpaths_json = [x for x in self._files_all if x.upper().endswith('.JSON')]
 
+        if len(fpaths_json) != len(fpaths_img):
+            log.warning(f'Number of files mismatch: JSON {len(fpaths_json)}  IMG {len(fpaths_img)}')
 
-    def _process_json_file(self, filename: str, filepath: str) -> None:
+        # Iterate json files to map the filenames
+        for fpath in fpaths_json:
+            fpath_img = fpath.replace('.JSON', '').replace('.json', '')  # Could be better - if more extensions
+            title = os.path.basename(fpath_img).replace('.jpg', '').replace('.JPG', '')
+            self._map_filepaths[title] = self.fpath_row(title=title, fpath_img=fpath_img, fpath_json=fpath)
+
+        # Check that all the inferred img jpaths exist
+        invalid_imgs = []
+        for img, row in self._map_filepaths.items():
+            if not os.path.exists(row.fpath_img):
+                log.warning(f'{row.fpath_img} does not exist!')
+                invalid_imgs.append(img)
+
+    def _process_json_file(self, title: str, fpath_row: fpath_row) -> None:
         """
         Load the given json file and look for the given jpg to edit it
-        :param filepath: json filepath
+        :param fpath_img: json filepath
         :return: None
         """
-        log.debug(f'Processing json {filepath}')
+        log.debug(f'Processing json {title}')
         try:
-            with open(filepath) as file:
+            with open(fpath_row.fpath_json) as json_data:
 
-                exif_dict = json.loads(file.read())
+                exif_dict = json.loads(json_data.read())
 
-            image_title = f'{exif_dict.get("title")}.json'
+            json_title = f'{exif_dict.get("title")}'
 
-            if image_title.upper() != filename.upper():
-                raise ValueError(f'Filename does not match title in EXIF JSON! {filename} : {image_title}')
+            if json_title.upper().replace('.jpg', '').replace('.JPG', '') != title.upper():
+                raise ValueError(f'Title does not match title in EXIF JSON! File: {title} : JSON title {json_title}')
 
-            if image_title in self._map_title2json.keys():
-                modifier = ExifModifier(image_filepath=filepath, target_exif=exif_dict)
-                modifier.modify()
+            modifier = ExifModifier(image_filepath=fpath_row.fpath_img, target_exif=exif_dict)
+            modifier.modify()
 
         except Exception as e:
-            log.warn(f'Could not process {filename} : {e}')
+            log.warning(f'Could not process {title} : {e}')
 
     def run(self) -> None:
         self._files_all = [os.path.join(self._dir_google, x) for x in os.listdir(self._dir_google)]
         self._check_extensions()
-        self._split_filepaths()
+        self._build_filepath_map()
 
-        for filename, filepath in self._map_title2json.items():
-            self._process_json_file(filename, filepath)
-
+        for title, row in self._map_filepaths.items():
+            self._process_json_file(title, row)
 
         t = ''
 
@@ -83,8 +96,7 @@ class Google2ICloudPhotos:
 if __name__ == '__main__':
     log.basicConfig(level=log.DEBUG)
 
-
-    google = r'C:\Users\Andrew Desktop\Pictures\google_photos\Takeout\Google Photos\Childhood'
+    google = r'C:\Users\Andrew Desktop\Pictures\google_photos\Takeout\Google Photos\Photos from 1970'
     icloud = ''
 
     app = Google2ICloudPhotos(dir_google=google,
